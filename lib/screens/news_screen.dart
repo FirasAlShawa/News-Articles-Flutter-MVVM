5b427f9f98582870129c6f6cba676561980c8913
FIRAS AL SHAWA <f84155439@china.huawei.com>
2020-08-21 19:49:56 +0300

    make the image fit the box and add radius

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,7 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/viewmodels/news_article_list_view_model.dart';
+import 'package:news_app/widgets/news_grid.dart';
 import 'package:provider/provider.dart';
 
 class NewsScreen extends StatefulWidget {
@@ -23,21 +23,10 @@ class _NewsScreenState extends State<NewsScreen> {
     var listViewModel = Provider.of<NewsArticleListViewModel>(context);
 
     return Scaffold(
-        appBar: AppBar(),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context, index) {
-            return GridTile(
-              child: Container(
-                child: CachedNetworkImage(
-                  imageUrl: listViewModel.articles[index].imageUrl,
-                ),
-              ),
-            );
-          },
-          itemCount: listViewModel.articles.length,
-        ));
+      appBar: AppBar(),
+      body: NewsGrid(
+        articles: listViewModel.articles,
+      ),
+    );
   }
 }
