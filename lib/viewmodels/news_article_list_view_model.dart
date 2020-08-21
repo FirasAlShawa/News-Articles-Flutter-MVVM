1a3a180c92f699089d1238f92826bb15fa35f621
FIRAS AL SHAWA <f84155439@china.huawei.com>
2020-08-21 19:06:40 +0300

    add new articles list viewmode

---
+++ lib/viewmodels/news_article_list_view_model.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:news_app/models/news_article.dart';
+import 'package:news_app/services/web_service.dart';
+import 'package:news_app/viewmodels/new_article_view_model.dart';
+
+enum LoadingStatus{
+  completed,
+  searching,
+  empty
+}
+
+class NewsArticleListViewModel with ChangeNotifier{
+  LoadingStatus loadingStatus = LoadingStatus.empty;
+  List<NewsArticleViewModel> articles = List<NewsArticleViewModel>();
+
+
+  void topHeadLines() async{
+    List<NewsArticle> newsArticles = await WebService().fetchTopHeadLines();
+    loadingStatus = LoadingStatus.searching;
+    notifyListeners();
+
+    this.articles = newsArticles.map((article) => NewsArticleViewModel(article: article)).toList();
+
+    if(this.articles.isEmpty){
+      this.loadingStatus = LoadingStatus.empty;
+    }else{
+      this.loadingStatus = LoadingStatus.completed;
+    }
+    notifyListeners();
+  }
+}
